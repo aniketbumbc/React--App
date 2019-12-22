dbf79eb82c982e7f6a1935ac71deb7ab62dca5b1
aniket umbc <aniket.umbc@gmail.com>
2019-12-22 13:37:49 +0530

    neasting component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+
+
+
+
 class App extends Component{
   render(){
   return(
     <div className="App">
     <h1>First React Project</h1>
     <p>Welcome To react world</p>
+    <Ninjas/>
     </div>
   );
 }
